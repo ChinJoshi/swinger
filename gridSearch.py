747c5cbd2cdd07a30b8d1a613d6822e02dfa78ce
chinmaya <cshjoshi@gmail.com>
2019-10-11 01:05:14 -0500

    made grid search the right version

--- gridSearch.py
+++ gridSearch.py
@@ -7,41 +7,27 @@ import glob
 import tensorflow
 from tensorflow import keras
 import multiprocessing
-
-# from keras.wrappers.scikit_learn import KerasClassifier
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
-# import talos as ta
 
 params = {
-    "batch_size": 256,  # 20<16<10, 25 was a bust
+    "batch_size": 256,
     "epochs": 500,
     "lr": 0.00010000,
     "time_steps": 60
 }
 
-""" grid = {
-    "1LSTM":[40,50,60,70,80,90,100,120,130],
-    "2Dropout": [0.3,0.4,0.5,0.6],
-    "3LSTM": [40,50,60,70,80,90,100,120,130],
-    "4Dropout": [0.3,0.4,0.5,0.6]
-} """
-
 grid = {
-    "1LSTM":[130],
+    "1LSTM":[60,70,80,90,100,110,120,130],
     "2Dropout": [0.3,0.4,0.5,0.6],
-    "3LSTM": [90,100,120,130],
+    "3LSTM": [60,70,80,90,100,110,120,130],
     "4Dropout": [0.3,0.4,0.5,0.6]
 }
 
 TIME_STEPS = params["time_steps"]
 BATCH_SIZE = params["batch_size"]
-
-INPUT_PATH = "D:\\GEModels\\geData.csv"
-
-TIME_STEPS = params["time_steps"]
-BATCH_SIZE = params["batch_size"]
+DATA_PATH = "geData.csv"
 
 def print_time(text, stime):
     seconds = (time.time()-stime)
@@ -49,9 +35,9 @@ def print_time(text, stime):
 
 #Make dataset fit into the timestep
 def trim_dataset(mat,batch_size):
-    no_of_rows_drop = mat.shape[0]%batch_size
+    no_of_rows_drop = mat.shape[0]%batch_size   #determine how many extra rows you have that don't fit into a multiple of batch
     if no_of_rows_drop > 0:
-        return mat[:-no_of_rows_drop]
+        return mat[:-no_of_rows_drop]   #cut that bitch off
     else:
         return mat
 
@@ -66,35 +52,35 @@ def build_timeseries(mat, y_col_index):
     to LSTM.
     """
     # total number of time-series samples would be len(mat) - TIME_STEPS
-    dim_0 = mat.shape[0] - TIME_STEPS
-    dim_1 = mat.shape[1]
-    x = np.zeros((dim_0, TIME_STEPS, dim_1))
-    y = np.zeros((dim_0,))
-    print("dim_0",dim_0)
-    for i in range(dim_0):
-        x[i] = mat[i:TIME_STEPS+i]
-        y[i] = mat[TIME_STEPS+i, y_col_index]
-    print("length of time-series i/o",x.shape,y.shape)
+    dim_0 = mat.shape[0] - TIME_STEPS   #determine number of inputs that have an output because you'll not have data timesteps forward once your index goes past len(mat.shape[0]) - TIME_STEPS
+    dim_1 = mat.shape[1]    #number of columns in dataset
+    x = np.zeros((dim_0, TIME_STEPS, dim_1))    #create an array with dimensions of viable inputs, the time step, the cols in the dataset
+    y = np.zeros((dim_0,))  #create an array length of viable inputs
+
+    for i in range(dim_0):  #for each viable input
+        x[i] = mat[i:TIME_STEPS+i]  #set the input array to a set of data the length of a timestep
+        y[i] = mat[TIME_STEPS+i, y_col_index]   #set output array that correalates with inout array to that data located in the y_col_index of the data
+    #print("length of time-series i/o",x.shape,y.shape)
     return x, y
 
 
 stime = time.time()
-df_ge = pd.read_csv(INPUT_PATH, engine='python')
-print(df_ge.shape)
-print(df_ge.columns)
-print(df_ge.head(5))
+df_ge = pd.read_csv(DATA_PATH, engine='python')    #read the csv file into a pandas dataset
 
-#df_ge = process_dataframe(df_ge)
-print(df_ge.dtypes)
-train_cols = ["Open","High","Low","Close","Volume"]
-df_train, df_test = train_test_split(df_ge, train_size=0.8, test_size=0.2, shuffle=False)
+#print(df_ge.shape)
+#print(df_ge.columns)
+#print(df_ge.head(5))
+#print(df_ge.dtypes)
+
+df_train, df_test = train_test_split(df_ge, train_size=0.8, test_size=0.2, shuffle=False)   #split the data into a traing set and validation set
 print("Train--Test size", len(df_train), len(df_test))
 
 # scale the feature MinMax, build array
-x = df_train.loc[:,train_cols].values
+train_cols = ["Open","High","Low","Close","Volume"]
+x = df_train.loc[:,train_cols].values   #grabs the data only(no headers) specified in train_cols
 min_max_scaler = MinMaxScaler()
-x_train = min_max_scaler.fit_transform(x)
-x_test = min_max_scaler.transform(df_test.loc[:,train_cols])
+x_train = min_max_scaler.fit_transform(x)   #Scale the training set and determine the scaling factors
+x_test = min_max_scaler.transform(df_test.loc[:,train_cols])    #Scale the the validation set using the precalculated scale factor
 
 print("Deleting unused dataframes of total size(KB)",(sys.getsizeof(df_ge)+sys.getsizeof(df_train)+sys.getsizeof(df_test))//1024)
 
@@ -104,75 +90,68 @@ del df_train
 del x
 
 print("Are any NaNs present in train/test matrices?",np.isnan(x_train).any(), np.isnan(x_train).any())
-x_t, y_t = build_timeseries(x_train, 3)
-x_t = trim_dataset(x_t, BATCH_SIZE)
-y_t = trim_dataset(y_t, BATCH_SIZE)
+
+x_t, y_t = build_timeseries(x_train, 3) #Build training dataset
+x_t = trim_dataset(x_t, BATCH_SIZE) #Make input data fit in the batch size
+y_t = trim_dataset(y_t, BATCH_SIZE) #Make output data fit in the batch size
 print("Batch trimmed size",x_t.shape, y_t.shape)
 
-x_temp, y_temp = build_timeseries(x_test, 3)
-x_val, x_test_t = np.split(trim_dataset(x_temp, BATCH_SIZE),2)
+x_temp, y_temp = build_timeseries(x_test, 3)    #Build validation dataset
+x_val, x_test_t = np.split(trim_dataset(x_temp, BATCH_SIZE),2)  
 y_val, y_test_t = np.split(trim_dataset(y_temp, BATCH_SIZE),2)
 
 print("Test size", x_test_t.shape, y_test_t.shape, x_val.shape, y_val.shape)
 
-def proc(dropout):
-    for layer_1_neurons in grid["1LSTM"]:
-        for layer_3_neurons in grid["3LSTM"]:
-            for layer_4_dropout in grid["4Dropout"]:
-                MODEL_NAME = ""
-                if layer_3_neurons != 0:
-                    MODEL_NAME = "GE_Model_B" + str(params["batch_size"]) + "_T" + str(params["time_steps"])+ "_L1N" + str(layer_1_neurons) + "_L2D" + str(dropout) + "_L3N" + str(layer_3_neurons) + "_L4D" + str(layer_4_dropout)
-                else:
-                    MODEL_NAME = "GE_Model_B" + str(params["batch_size"]) + "_T" + str(params["time_steps"])+ "_L1N" + str(layer_1_neurons) + "_L2D" + str(dropout)
+dropout = 0.6
+for layer_1_neurons in grid["1LSTM"]:
+    for layer_3_neurons in grid["3LSTM"]:
+        for layer_4_dropout in grid["4Dropout"]:
+            MODEL_NAME = ""
+            if layer_3_neurons != 0:
+                MODEL_NAME = "GE_Model_B" + str(params["batch_size"]) + "_T" + str(params["time_steps"])+ "_L1N" + str(layer_1_neurons) + "_L2D" + str(dropout) + "_L3N" + str(layer_3_neurons) + "_L4D" + str(layer_4_dropout)
+            else:
+                MODEL_NAME = "GE_Model_B" + str(params["batch_size"]) + "_T" + str(params["time_steps"])+ "_L1N" + str(layer_1_neurons) + "_L2D" + str(dropout)
 
-                OUTPUT_PATH = "D:\\GEModels\\" + MODEL_NAME
+            OUTPUT_PATH = "GEModels/" + MODEL_NAME
+            if not os.path.exists(OUTPUT_PATH):
+                os.makedirs(OUTPUT_PATH)
+            else:
+                print("Output directory exists,exiting to prevent overwriting")
+                quit()
 
-                if not os.path.exists(OUTPUT_PATH):
-                    os.makedirs(OUTPUT_PATH)
-                else:
-                    print("Output directory exists,exiting to prevent overwriting")
-                    quit()
+            lstm_model = keras.Sequential()
+            if layer_3_neurons == 0:
+                #generate model without lstm3 or dropout4
+                lstm_model.add(keras.layers.CuDNNLSTM(layer_1_neurons, batch_input_shape=(BATCH_SIZE, TIME_STEPS, x_t.shape[2]), stateful=True, return_sequences=True, kernel_initializer='random_uniform'))
 
-                lstm_model = keras.Sequential()
-                if layer_3_neurons == 0:
-                    #generate model without lstm3 or dropout4
-                    lstm_model.add(keras.layers.CuDNNLSTM(layer_1_neurons, batch_input_shape=(BATCH_SIZE, TIME_STEPS, x_t.shape[2]), stateful=True, return_sequences=True, kernel_initializer='random_uniform'))
+                lstm_model.add(keras.layers.Dropout(dropout))
 
-                    lstm_model.add(keras.layers.Dropout(dropout))
+                lstm_model.add(keras.layers.Dense(20,activation='relu'))
 
-                    lstm_model.add(keras.layers.Dense(20,activation='relu'))
+                lstm_model.add(keras.layers.Dense(1,activation='sigmoid'))
 
-                    lstm_model.add(keras.layers.Dense(1,activation='sigmoid'))
+                optimizer = keras.optimizers.RMSprop(lr=params["lr"])
+                lstm_model.compile(loss='mean_squared_error', optimizer=optimizer)
 
-                    optimizer = keras.optimizers.RMSprop(lr=params["lr"])
-                    lstm_model.compile(loss='mean_squared_error', optimizer=optimizer)
+                mcp = keras.callbacks.ModelCheckpoint(os.path.join(OUTPUT_PATH, MODEL_NAME+".{epoch:02d}-{val_loss:.5f}.hdf5"), monitor='val_loss', verbose=2, save_best_only=False, save_weights_only=False, mode='auto', period=10)
+                csv_logger = keras.callbacks.CSVLogger(OUTPUT_PATH+"/"+MODEL_NAME+".log", append=True)
+                lstm_model.fit(x_t, y_t, epochs=params["epochs"], verbose=2, batch_size=BATCH_SIZE,shuffle=False, validation_data=(trim_dataset(x_val, BATCH_SIZE), trim_dataset(y_val, BATCH_SIZE)), callbacks=[mcp,csv_logger])
+            else:
+                #generate model using layer_1_neurons, 2dropout of 0.3, layer_3_neurons, and layer_4_dropout
+                lstm_model.add(keras.layers.CuDNNLSTM(layer_1_neurons, batch_input_shape=(BATCH_SIZE, TIME_STEPS, x_t.shape[2]), stateful=True, return_sequences=True, kernel_initializer='random_uniform'))
 
-                    mcp = keras.callbacks.ModelCheckpoint(os.path.join(OUTPUT_PATH, MODEL_NAME+".{epoch:02d}-{val_loss:.5f}.hdf5"), monitor='val_loss', verbose=1, save_best_only=False, save_weights_only=False, mode='auto', period=10)
-                    csv_logger = keras.callbacks.CSVLogger(OUTPUT_PATH+"\\"+MODEL_NAME+".log", append=True)
-                    
-                    lstm_model.fit(x_t, y_t, epochs=params["epochs"], verbose=2, batch_size=BATCH_SIZE,shuffle=False, validation_data=(trim_dataset(x_val, BATCH_SIZE), trim_dataset(y_val, BATCH_SIZE)), callbacks=[mcp,csv_logger])
-                else:
-                    #generate model using layer_1_neurons, 2dropout of 0.3, layer_3_neurons, and layer_4_dropout
-                    lstm_model.add(keras.layers.CuDNNLSTM(layer_1_neurons, batch_input_shape=(BATCH_SIZE, TIME_STEPS, x_t.shape[2]), stateful=True, return_sequences=True, kernel_initializer='random_uniform'))
+                lstm_model.add(keras.layers.Dropout(dropout))
 
-                    lstm_model.add(keras.layers.Dropout(dropout))
+                lstm_model.add(keras.layers.CuDNNLSTM(layer_3_neurons))
 
-                    lstm_model.add(keras.layers.CuDNNLSTM(layer_3_neurons))
+                lstm_model.add(keras.layers.Dropout(layer_4_dropout))
 
-                    lstm_model.add(keras.layers.Dropout(layer_4_dropout))
+                lstm_model.add(keras.layers.Dense(20,activation='relu'))
 
-                    lstm_model.add(keras.layers.Dense(20,activation='relu'))
+                lstm_model.add(keras.layers.Dense(1,activation='sigmoid'))
 
-                    lstm_model.add(keras.layers.Dense(1,activation='sigmoid'))
-
-                    optimizer = keras.optimizers.RMSprop(lr=params["lr"])
-                    lstm_model.compile(loss='mean_squared_error', optimizer=optimizer)
-                    mcp = keras.callbacks.ModelCheckpoint(os.path.join(OUTPUT_PATH, MODEL_NAME+".{epoch:02d}-{val_loss:.5f}.hdf5"), monitor='val_loss', verbose=1, save_best_only=False, save_weights_only=False, mode='auto', period=10)
-                    csv_logger = keras.callbacks.CSVLogger(OUTPUT_PATH+"\\"+MODEL_NAME+".log", append=True)                        
-                    lstm_model.fit(x_t, y_t, epochs=params["epochs"], verbose=2, batch_size=BATCH_SIZE,shuffle=False, validation_data=(trim_dataset(x_val, BATCH_SIZE), trim_dataset(y_val, BATCH_SIZE)), callbacks=[mcp,csv_logger])
-
-if __name__ == '__main__':
-    #proc(0.3)
-    proc(0.4)
-    proc(0.5)
-    proc(0.6)
+                optimizer = keras.optimizers.RMSprop(lr=params["lr"])
+                lstm_model.compile(loss='mean_squared_error', optimizer=optimizer)
+                mcp = keras.callbacks.ModelCheckpoint(os.path.join(OUTPUT_PATH, MODEL_NAME+".{epoch:02d}-{val_loss:.5f}.hdf5"), monitor='val_loss', verbose=2, save_best_only=False, save_weights_only=False, mode='auto', period=10)
+                csv_logger = keras.callbacks.CSVLogger(OUTPUT_PATH+"/"+MODEL_NAME+".log", append=True)                        
+                lstm_model.fit(x_t, y_t, epochs=params["epochs"], verbose=2, batch_size=BATCH_SIZE,shuffle=False, validation_data=(trim_dataset(x_val, BATCH_SIZE), trim_dataset(y_val, BATCH_SIZE)), callbacks=[mcp,csv_logger])
